8b9467c6bd3f14339c63ac808bd6181454c4f94b
Tyler M <J.t.moore4700@gmail.com>
2023-08-22 23:58:08 -0400

    worked on html

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { planet } from "./utilities/tags";
 import { commodity } from "./utilities/commodities";
 
-console.log(planet, commodity)
+console.log(
+    `This world is ${planet.world?.description}.
+     The atmosphere is ${planet.atmos?.description}.
+     The temperature is ${planet.temp?.description}.
+     The biosphere is ${planet.bios?.description}.
+     The population is ${planet.pop?.description}.
+     The tech level is ${planet.tchlvl?.description}.
+     The trade nature is ${planet.trade?.description}.
+     The local commodity is ${commodity?.cargo}, ${commodity?.cost} credits, ${commodity?.techLevel}, ${commodity?.type}.`
+ );
