167eb7fa2cc322200f177c4dac6d8d88c4f08ca4
Tyler M <J.t.moore4700@gmail.com>
2023-08-29 00:06:28 -0400

    Error Handling on define.ts, trade_selector.ts, and roll_die.ts

--- src/generators/define.ts
+++ src/generators/define.ts
@@ -1,11 +1,13 @@
-import { Result, Ok, Err } from "ts-results";
 import { ITag } from "../utilities/ITag";
+import { Result, Ok, Err } from "ts-results";
+import { getRandomIntInclusive } from "../utilities/roll_die";
 
 function Definer(tags: ITag[]): Result<ITag, string> {
     if (tags.length === 0) return Err(`Empty Array`)
-    const result = tags.at(Math.floor(Math.random() * tags.length))
-    if (!result) return Err(`undefined`)
+    const dieRoll = getRandomIntInclusive(tags.length - 1, 0)
+    if (dieRoll.err) return Err(`getRandomIntInclusive produced an error. Verify Array size.`)
+    const result = tags[dieRoll.val]
+    if (!result) return Err(`undefined, check the data_table folder to verify integrity`)
     return Ok(result)
 }
-
 export { Definer }
--- src/generators/trade_selector.ts
+++ src/generators/trade_selector.ts
@@ -3,6 +3,7 @@ import { ITradeTag } from "../data_table/trade_tags";
 import { Result, Ok, Err } from "ts-results";
 
 function tradeSelector(highTradeTag: ITradeTag[], lowTradeTag: ITradeTag[]): Result<ITradeTag, string> {
+    if (highTradeTag.length === 0 || lowTradeTag.length === 0) return Err(`Empty Array`)
     if (coinFlip()) { 
         const selectedTrade = getRandomIntInclusive(highTradeTag.length)
         if (selectedTrade.err) return Err(`Array sizing error`)
