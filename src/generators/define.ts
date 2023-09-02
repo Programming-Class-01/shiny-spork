bf215b7370de42209664823aeac8c6458fb84db2
Tyler M <J.t.moore4700@gmail.com>
2023-09-01 22:41:51 -0400

    Correct code

--- src/generators/define.ts
+++ src/generators/define.ts
@@ -2,12 +2,12 @@ import { IUnion } from "../utilities/ITag";
 import { Result, Ok, Err } from "ts-results";
 import { getRandomIntInclusive } from "../utilities/roll_die";
 
-function Definer(tags: NonNullable<IUnion>[]): Result<IUnion, string> {
-    let localTags = Object.freeze(tags)
-    if (localTags.length === 0) return Err(`Empty Array`)
-    const dieRoll = getRandomIntInclusive(localTags.length - 1, 0)
+function Definer(tags: IUnion[]): Result<IUnion, string> {
+    if (tags.length === 0) return Err(`Empty Array`)
+    const dieRoll = getRandomIntInclusive(tags.length - 1, 0)
     if (dieRoll.err) return Err(`getRandomIntInclusive produced an error. Verify Array size.`)
-    if (!localTags[dieRoll.val]) return Err(`undefined, check the data_table folder to verify integrity`)
-    return Ok(localTags[dieRoll.val])
+    const result = tags[dieRoll.val]
+    if (!result) return Err(`undefined, check the data_table folder to verify integrity`)
+    return Ok(result)
 }
 export { Definer }
